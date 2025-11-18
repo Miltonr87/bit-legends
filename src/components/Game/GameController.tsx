934b7813083fa855382fe6fc5476dc532d3fa0d4
Miltonr87 <miltonrodrigues713@gmail.com>
2025-11-18 20:27:31 -0300

    Added modal to show control

--- src/components/Game/GameController.tsx
+++ src/components/Game/GameController.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@/components/ui/card';
 import { motion } from 'framer-motion';
 import { Gamepad2, Keyboard } from 'lucide-react';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 import gamepad from '../../../public/assets/backgrounds/controller_min.jpg';
 
 export const GameController = () => {
@@ -15,7 +16,7 @@ export const GameController = () => {
           <div className="flex items-center gap-3">
             <Gamepad2 className="h-6 w-6 text-accent animate-pulse" />
             <h2 className="text-lg sm:text-xl font-bold text-foreground">
-              Controller
+              Controls
             </h2>
           </div>
           <div className="hidden sm:flex items-center gap-2 text-sm text-muted-foreground pr-2">
@@ -23,13 +24,26 @@ export const GameController = () => {
             Keyboard Default
           </div>
         </div>
-        <div className="rounded-xl overflow-hidden border-2 border-accent/20 shadow-xl">
-          <img
-            src={gamepad}
-            alt="Gamepad Illustration"
-            className="object-cover w-full h-[300px] sm:h-[340px]"
-          />
-        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <div className="rounded-xl overflow-hidden border-2 border-accent/20 shadow-xl cursor-pointer hover:opacity-90 transition">
+              <img
+                src={gamepad}
+                alt="Gamepad Illustration"
+                className="object-cover w-full h-[300px] sm:h-[340px]"
+              />
+            </div>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-2 shadow-none">
+            <div className="flex items-center justify-center">
+              <img
+                src={gamepad}
+                alt="Gamepad Fullsize"
+                className="rounded-lg object-contain max-w-[600px] max-h-[600px] w-full h-full"
+              />
+            </div>
+          </DialogContent>
+        </Dialog>
       </Card>
     </motion.div>
   );
